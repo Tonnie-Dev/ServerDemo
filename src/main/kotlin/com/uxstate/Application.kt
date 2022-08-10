23f3740d444ad17d97376cb2c0db490bff13d2f8
Tonnie <vontonnie@gmail.com>
2022-08-10 07:53:21 +0300

    Add module Application class extension function

--- src/main/kotlin/com/uxstate/Application.kt
+++ src/main/kotlin/com/uxstate/Application.kt
@@ -1,3 +1,13 @@
 package com.uxstate
 
+import io.ktor.server.application.*
+
+
 fun main(args:Array<String>):Unit = io.ktor.server.netty.EngineMain.main(args)
+
+
+//Ext fxn on the Application class
+
+fun Application.module( ){
+    
+}
