26da3df59c22e1fad924ee59abe81757199884d9
Tonnie <vontonnie@gmail.com>
2022-08-10 08:13:44 +0300

    Configure the GET http request on module ext fxn

--- src/main/kotlin/com/uxstate/Application.kt
+++ src/main/kotlin/com/uxstate/Application.kt
@@ -1,6 +1,8 @@
 package com.uxstate
 
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 
 fun main(args:Array<String>):Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -8,6 +10,17 @@ fun main(args:Array<String>):Unit = io.ktor.server.netty.EngineMain.main(args)
 
 //Ext fxn on the Application class
 
+
+//Our engineMain will use this module to respond to clients
 fun Application.module( ){
+
+    routing {
+        //server root path
+        get (path = "/"){
+
+            //define how to respond to user's requests
+            call.respondText("Hello Tonnie,")
+        }
+    }
 
 }
