cc1211f048fdfe7e27eec5c048fc7d639d516f62
Tonnie <vontonnie@gmail.com>
2022-08-10 07:32:00 +0300

    Add Application Block specifying the server engine to use

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,20 @@ repositories {
     mavenCentral()
 }
 
+//application block with the server engine to use
+application{
+
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
 dependencies {
     testImplementation(kotlin("test"))
 
-    //Core dependencies
+    //Core dependencies - basic functionality for Ktor Server
     implementation("io.ktor:ktor-server-core:2.0.3")
     implementation("io.ktor:ktor-server-netty:2.0.3")
 
-    //Logging dependency
+    //Logging dependency - see logs from the server
     implementation("ch.qos.logback:logback-classic:1.2.11")
 }
 
